6f91be49909c8d59395cfc5766e24765d03d1591
fernandovidalpiazza <fervidalpiazza@gmail.com>
2023-08-02 20:02:58 -0300

    infosenci

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOvbzgKDGCI1q6wR1jNuLPmjkYgDx79kY",
-  authDomain: "curso-react-coder-ad063.firebaseapp.com",
-  projectId: "curso-react-coder-ad063",
-  storageBucket: "curso-react-coder-ad063.appspot.com",
-  messagingSenderId: "1068744433796",
-  appId: "1:1068744433796:web:6300f4193295b5df1c22d1"
+  apiKey:import.meta.VITE_apiKey ,
+  authDomain: import.meta.VITE_authDomain ,
+  projectId:import.meta.VITE_projectId ,
+  storageBucket:import.meta.VITE_storageBucket ,
+  messagingSenderId:import.meta.VITE_messagingSenderId ,
+  appId:import.meta.VITE_appId ,
 };
 
 const app = initializeApp(firebaseConfig);
